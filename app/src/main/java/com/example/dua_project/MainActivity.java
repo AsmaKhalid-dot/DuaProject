9d3bf9b272635e31e6aa3bcfb4f3540eba4a4f02
unknown <asmak9060@gamil.com>
2021-07-04 08:06:23 +0500

    2nd commit

--- app/src/main/java/com/example/dua_project/MainActivity.java
+++ app/src/main/java/com/example/dua_project/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.dua_project;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -28,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
                 return view;
             }
         };
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = listview.getItemAtPosition(position).toString();
+                /*Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+                intent.putExtra("pdfFileNames",item);
+                startActivity(intent);*/
+            }
+        });
     }
 }
