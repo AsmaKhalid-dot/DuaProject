b86432d56f771c42ea4c9989530bf98fc9ac4a10
unknown <asmak9060@gamil.com>
2021-07-04 08:12:58 +0500

    6th commit

--- app/src/main/java/com/example/dua_project/MainActivity2.java
+++ app/src/main/java/com/example/dua_project/MainActivity2.java
@@ -12,5 +12,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        pdfView=(PDFView)findViewById(R.id.opener);
+        String getitem=getIntent().getStringExtra("pdfFileNames");
     }
 }
